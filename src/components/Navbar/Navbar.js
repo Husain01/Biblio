86f99a4fa5c6cf0ad264c6d428ec2ced4316f925
Husain Bhagat <hussainbhagat52@gmail.com>
2022-05-18 12:39:12 +0530

    Refactor: Use module css in Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,21 +1,26 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 import logo from '../../assets/logo.png';
 import logo_txt from '../../assets/logo-txt.png';
-import './Navbar.css';
+import styles from "./Navbar.module.css";
 
 export const Navbar = () => {
   return (
-    <header class="navbar">
-      <div class="logo">
-        <img src={logo} alt="" />
-        <img src={logo_txt} alt="" />
+    <header class={`navbar ${styles.navbar}`}>
+        <Link to="/">
+      <div class={`logo ${styles.logo}`}>
+        <img src={logo} alt="" className={styles.logoImg}/>
+        <img src={logo_txt} alt="" className={styles.logoImg}/>
       </div>
+        </Link>
       <div class="search">
         <i class="fas fa-search"></i>
         <input type="text" class="search-input" placeholder="Search" />
       </div>
       <div class="nav-links">
+          <Link to='/login'>
         <button class="btn btn-primary btn-login normal-shadow">Login</button>
+          </Link>
       </div>
     </header>
   );

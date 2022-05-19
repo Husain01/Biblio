056ea5f13ad2527cfe9a5c0dc585cab809c4a7cb
Husain Bhagat <hussainbhagat52@gmail.com>
2022-05-19 09:34:21 +0530

    Feat: Add Auth Reducer

---
+++ src/Reducer/Auth/AuthReducer.js
@@ -0,0 +1,21 @@
+
+export const AuthReducer = (state, action) => {
+    switch (action.type) {
+        case "LOGIN":
+        case "SIGNUP":
+        case "CHECKUSER":
+            return {
+                ...state,
+                user: action.payload.user,
+                token: action.payload.token
+            }
+        case "LOGOUT":
+            return {
+                ...state,
+                user: null,
+                token: null
+            }
+            default:
+                return state;
+    }
+}

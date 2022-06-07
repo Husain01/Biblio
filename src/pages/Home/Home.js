a76bf226ba32968ce6dadcae221e7c9a5f81c20b
Husain Bhagat <hussainbhagat52@gmail.com>
2022-06-07 15:52:50 +0530

    Feat: Add Category Cards

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,19 +1,41 @@
-import React from 'react';
-import styles from './Home.module.css';
+import React from "react";
+import styles from "./Home.module.css";
+import HPImg from '../../assets/HarryPotterCategoryImage.jpg';
+import GOTImg from '../../assets/GOTCategoryImage.jpg';
 
 export const Home = () => {
   return (
     <div className="content-container">
-        <div className={`main-content ${styles.mainContent}`}>
+      <div className={`main-content ${styles.mainContent}`}>
+        <h3>Choose your Quiz</h3>
         <div className={`${styles.cardWrapper}`}>
-        <div className="card">
-            <div className="card-img">
-                <img src="" alt="" />
+          <div className={`card ${styles.card} normal-shadow`}>
+            <div className={styles.cardImg}>
+              <img src={HPImg} alt="" />
             </div>
+            <h5 className={styles.cardText}>Get your thinking spells ready!</h5>
+            <button
+              className={`btn btn-primary ${styles.btnPlay} normal-shadow`}
+            >
+              Let's Play
+            </button>
+          </div>
+          <div className={`card ${styles.card} normal-shadow`}>
+            <div className={styles.cardImg}>
+              <img
+                src={GOTImg}
+                alt=""
+              />
+            </div>
+            <h5 className={styles.cardText}>You Win or You Die!</h5>
+            <button
+              className={`btn btn-primary ${styles.btnPlay} normal-shadow`}
+            >
+              Let's Play
+            </button>
+          </div>
         </div>
-        <div className="card">Two</div>
-        </div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};

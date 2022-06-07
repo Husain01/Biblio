889d6d4c3104210e1d565cc24c1e49d65834c430
Husain Bhagat <hussainbhagat52@gmail.com>
2022-06-07 11:47:08 +0530

    Create RequiresAuth.js

---
+++ src/RequiresAuth.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navigate, useLocation } from 'react-router-dom'
+import { useAuth } from './context/Auth/AuthContext';
+
+export const RequiresAuth = ({children}) => {
+  const location = useLocation();
+  const {authStateL: {token}} = useAuth();
+    return token ? (children) : (
+        <Navigate to='/login' state={{from: location}} replace />
+    )
+
+}
